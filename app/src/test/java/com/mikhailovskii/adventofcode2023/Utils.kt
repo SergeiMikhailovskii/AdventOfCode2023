be4541d11601a3d1e633e4e0c464fd7d99f1ad48
sergei-mikhailovskii-idf <sergei.mikhailovskii@idfinance.com>
2023-12-03 19:29:09 +0300

    Day 1

---
+++ app/src/test/java/com/mikhailovskii/adventofcode2023/Utils.kt
@@ -0,0 +1,30 @@
+package com.mikhailovskii.adventofcode2023
+
+import java.math.BigInteger
+import java.security.MessageDigest
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+
+/**
+ * Reads lines from the given input txt file.
+ */
+fun readInput(name: String) = Path("src/test/java/com/mikhailovskii/adventofcode2023/$name.txt").readLines()
+
+/**
+ * Converts string to md5 hash.
+ */
+fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
+    .toString(16)
+    .padStart(32, '0')
+
+/**
+ * The cleaner shorthand for printing output.
+ */
+fun Any?.println() = println(this)
+
+fun Regex.findAllWithOverlap(input: CharSequence, startIndex: Int = 0): Sequence<MatchResult> {
+    if (startIndex < 0 || startIndex > input.length) {
+        throw IndexOutOfBoundsException("Start index out of bounds: $startIndex, input length: ${input.length}")
+    }
+    return generateSequence({ find(input, startIndex) }, { find(input, it.range.first + 1) })
+}
---
+++ app/src/test/java/com/mikhailovskii/adventofcode2023/day1/Day1.kt
@@ -0,0 +1,36 @@
+package com.mikhailovskii.adventofcode2023.day1
+
+import com.mikhailovskii.adventofcode2023.println
+import com.mikhailovskii.adventofcode2023.readInput
+import org.junit.Test
+
+class Day1 {
+    @Test
+    fun part1() {
+        val input = readInput("day1/input1")
+        val result = input.sumOf {
+            (it.first { it.isDigit() }.toString() + it.last { it.isDigit() }).toInt()
+        }
+        println(result)
+    }
+
+    @Test
+    fun part2() {
+        val input = readInput("day1/input2")
+        val numbersInStr = arrayOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+        val regex = "\\d|one|two|three|four|five|six|seven|eight|nine".toRegex()
+        fun toNumber(string: String): Int {
+            val index = numbersInStr.indexOf(string)
+            return if (index == -1) string.toInt()
+            else index + 1
+        }
+        input.sumOf {
+            val all = regex.findAllWithOverlap(it)
+            val first = all.first().value
+            val last = all.last().value
+            (toNumber(first) * 10 + toNumber(last)).apply {
+                println()
+            }
+        }.println()
+    }
+}
